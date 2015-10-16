31674e6cdd85fb0a2a0764810c11211b542fd033
jack-gillette <jackackckk@gmail.com>
2015-10-15 20:25:51 -0400

    Add the basic interfaces for interaction between the parts of the application -- see comments inside

---
+++ src/DrawCommand.java
@@ -0,0 +1,9 @@
+package SLOGO;
+
+public interface DrawCommand {
+
+	// The Drawable object calls commands that implement this interface, which are then passed to the client which passes them to the view.
+
+	void draw(DrawView view);
+
+}
---
+++ src/ParserClient.java
@@ -0,0 +1,21 @@
+package SLOGO;
+
+
+public interface ParserClient {
+
+	// The general application manager implements this, is passed into parser as type ParserClient.
+	// This is how the parser interacts with the manager.
+
+	double getX();
+
+	double getY();
+
+	double getHeading();
+
+	Boolean getPenUp();
+
+	Boolean getShowing();
+
+	void postCommand(ParserCommand cmd);
+
+}
