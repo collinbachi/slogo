755653d1ac1c46d9d212ca69c1501de4249e2370
Matt Battles <matthewjbattles@gmail.com>
2015-10-27 00:03:27 -0400

    Added ColorPicker.

---
+++ src/view/ColorPalette.java
@@ -0,0 +1,9 @@
+package view;
+
+import javafx.scene.control.ColorPicker;
+import javafx.scene.paint.Color;
+
+public class ColorPalette {
+	private ColorPicker cp = new ColorPicker(Color.BLUE);
+
+}
