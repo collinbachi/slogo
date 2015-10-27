dc7d523c06c498462ab2afee629acb26a959e82d
Matt Battles <matthewjbattles@gmail.com>
2015-10-27 01:25:44 -0400

    Color picker refinements.

--- src/view/ColorPalette.java
+++ src/view/ColorPalette.java
@@ -1,9 +1,24 @@
 package view;
 
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.TextArea;
 import javafx.scene.paint.Color;
 
-public class ColorPalette {
-	private ColorPicker cp = new ColorPicker(Color.BLUE);
-
+public class ColorPalette extends ConcreteView {
+	
+	private static final double CP_X = 95;
+	private static final double CP_Y = 95;
+	private static final double CP_W = 25;
+	private static final double CP_H = 65;
+	
+	private ColorPicker cp;
+	
+	public ColorPalette(){
+		super();
+		this.cp = new ColorPicker(Color.BLUE);
+		cp.setLayoutX(X(CP_X));
+		cp.setLayoutY(Y(CP_Y));
+		cp.setPrefWidth(W(CP_W));
+		cp.setPrefHeight(H(CP_H));
+	}
 }
